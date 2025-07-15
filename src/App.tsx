bd00a13efbab9a1f8c42a5024ff2c3425d941669
Abhishek <abhisek85400@gmail.com>
2025-07-15 14:25:20 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Main from './components/main';
+import CheckoutPage from './Checkout';
 
 function App() {
+  // Extract query params from window.location
+  const queryParams = new URLSearchParams(window.location.search);
+  const showCheckout = queryParams.get("checkout") === "true";
+
   return (
     <div className="App">
-      <Main/>
+      {showCheckout ? <CheckoutPage /> : <Main />}
     </div>
   );
 }
